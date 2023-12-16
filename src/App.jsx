22845923dd188297c69c675e8d3a72e5fab7a48c
nathanieladiah <nathanieladiah@gmail.com>
2023-12-15 20:50:06 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/sign-in" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+export default App;
